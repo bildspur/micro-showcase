0cc266bd266dd66ccc1997cd49770435a7f10c1c
Florian Bruggisser <florian@nexpose.ch>
2017-11-17 11:44:58 +0100

    implemented micro palette and palette switching

--- src/ColorScene.cpp
+++ src/ColorScene.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "ColorScene.h"
+
+ColorScene::ColorScene() {
+
+}
+
+void ColorScene::setup(LEDRingPtr *rings) {
+    LightScene::setup(rings);
+
+    // setup microPalette
+    fill_solid(microPalette, 16, CRGB::Gray);
+    for (int i = 0; i < 12; i++) {
+        microPalette[i] = CRGB::Cyan;
+    }
+
+    changePalette();
+}
+
+void ColorScene::loop() {
+    LightScene::loop();
+
+    if(timer.elapsed())
+    {
+        // change palette
+        paletteIndex = (paletteIndex + 1) % 2;
+        changePalette();
+    }
+
+    startIndex = (startIndex + 1) % 14400; /* motion speed */
+
+    // set rings
+    uint8_t colorIndex = startIndex;
+    for (uint8_t i = 0; i < sizeof(rings) + 1; i++) {
+        for( int j = 0; j < rings[i]->length; j++) {
+            rings[i]->leds[j] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+            colorIndex += 3;
+        }
+    }
+}
+
+void ColorScene::changePalette() {
+    currentBlending = LINEARBLEND;
+
+    if(paletteIndex == 0) {currentPalette = RainbowColors_p;}
+    if(paletteIndex == 1) {currentPalette = microPalette;}
+}
--- src/ColorScene.h
+++ src/ColorScene.h
@@ -6,8 +6,33 @@
 #define MICRO_SHOWCASE_COLORSCENE_H
 
 
-class ColorScene {
+#include "LightScene.h"
+#include "Timer.h"
 
+#define PALETTE_TIME (1000 * 60 * 5)
+
+class ColorScene : public LightScene {
+private:
+    CRGBPalette16 currentPalette;
+    TBlendType currentBlending;
+
+    CRGBPalette16 microPalette;
+
+    uint8_t startIndex = 0;
+    uint8_t brightness = 255;
+
+    uint8_t paletteIndex = 0;
+
+    Timer timer = Timer(static_cast<unsigned long>(PALETTE_TIME));
+
+public:
+    ColorScene();
+
+    void setup(LEDRingPtr *rings) override;
+
+    void loop() override;
+
+    void changePalette();
 };
 
 
